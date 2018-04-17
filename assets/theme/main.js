948374519217c5f05a5b7abba2fc8624063d167e
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-04-16 23:01:25 -0500

    Se agrega la pagina 4

--- assets/theme/main.js
+++ assets/theme/main.js
@@ -3,12 +3,34 @@ $(document).ready( function() {
         let classText = 'item-select'
         if( $(this).hasClass(classText)) {
             $(this).removeClass(classText)
+            
             $(this).find('.tarjeta1').addClass('d-none')
-           
             return;
         }
 
         $(this).find('.tarjeta1').removeClass('d-none')
         $(this).addClass(classText)
     })
+
+    //Tipo de Pago
+    $(".selected1").click(function(event){
+        if(! $(this).hasClass('btn-success')) $(".selected1").addClass('btn-success')
+        $(".selected2").removeClass('btn-success')
+        $(".selected3").removeClass('btn-success')
+    })
+
+
+    $(".selected2").click(function(event){
+        if(! $(this).hasClass('btn-success')) $(".selected2").addClass('btn-success')
+        $(".selected1").removeClass('btn-success')
+        $(".selected3").removeClass('btn-success')
+    })
+
+
+    $(".selected3").click(function(event){
+        if(! $(this).hasClass('btn-success')) $(".selected3").addClass('btn-success')
+        $(".selected1").removeClass('btn-success')
+        $(".selected2").removeClass('btn-success')
+    })
+
 })
